3d1e002f132f47bd4e6d4c45a55936bbd89e561f
yyi <yyi@uw.edu>
2020-02-20 23:05:09 -0800

    add new route

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask
-__author__ = 'yyi'
+'''
+__author__ == 'yyiust'
+'''
+from flask import Flask, make_response
 app = Flask(__name__)
+app.config.from_object('config')
 
 
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+        q: keyword/isbn
+        page
+    :return:
+    """
+    # isbn: isbn13: 13 digits
+    # isbn10: 10digits and '-'
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+    short_q = q.replace('-', '')
+    if '-' in q and len(short_q) == 10 and short_q.isdigit():
+        isbn_or_key = 'isbn'
 
-@app.route('/')
-def hello_world():
-    # class-based view
-    return 'Hello World!'
+    pass
+
+# @app.route('/')
+# def hello_world():
+#     headers = {
+#         'content-type': 'text/html'
+#     }
+#     response = make_response('<html></html>', 404)
+#     response.headers = headers
+#     # class-based view
+#     return response
 
 # app.add_url_rule('/', view_func=hello_world)
-
+# app.run(debug=app.config['DEBUG'])
 if __name__ == '__main__':
-    app.run(debug=True)
+    # only run in this file, if this file is imported as module, will not execute
+    # production environment, need use nginx + uwsgi(web server)
+    # ensure this will not run in production environment
+    app.run(debug=app.config['DEBUG'])
--- readme.md
+++ readme.md
@@ -1,3 +1,34 @@
 # flask
 > unique url
->
+
+## register a route
+
+- use python decorator, for example
+```python
+@app.route('/hello')
+def hello_word:
+    return 'hello word'
+```
+
+- use methods
+```python
+app.add_url_rules('/hello', view_func=hello_world)
+```
+
+### parameters in `run`
+- `host`: IP address: use mask '0.0.0.0', means can be accessed by computer from out web
+- `debug`: `boolean`, set debug/production mode
+- `port`
+
+
+### config flle
+- can import as a model
+- use `app.config.from_object()`, accept the path of the config module
+> app.config is the subclass of dict, so it can be retrieved as a dictionary
+
+
+### view function
+- status code: 200, 404, 301
+- content-type: =text/html http headers, how to parser the returned text
+- return a `Response` object
+- `make_response` to create
